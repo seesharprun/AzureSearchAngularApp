6514ea9323256ea6d6a525115b009f00666b73d2
Sidney Andrews <sidney@seesharprun.net>
2015-02-06 20:54:45 -0500

    50% complete with fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,13 @@ module.exports = function (grunt) {
     pkg: packagejson,
     distDir: 'web/',
     devDir: 'dev/',
+    tempDir: 'tmp/',
     jsDir: 'js/',
     cssDir: 'css/',
     htmlDir: 'html/',
+    fontsDir: 'fonts/',
     bwr: {
-      name: 'bower'
+      name: 'Bower'
     },
     concat: {
       js: {
@@ -19,27 +21,25 @@ module.exports = function (grunt) {
         dest: '<%= distDir %><%= jsDir %><%= pkg.name %>.js'
       },
       css: {
-        src: ['<%= distDir %><%= cssDir %>**/*.css'],
+        src: ['<%= devDir %><%= cssDir %>**/*.css'],
         dest: '<%= distDir %><%= cssDir %><%= pkg.name %>.css'
       }
     },
     bower_concat: {
       all: {
         dest: '<%= distDir %><%= jsDir %><%= bwr.name %>.js',
-        cssDest: '<%= distDir %><%= cssDir %><%= bwr.name %>.css',
-        bowerOptions: {
-          relative: false
-        }
+        cssDest: '<%= distDir %><%= cssDir %><%= bwr.name %>.css'
       }
     },
     uglify: {
      options: {
-       banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+       banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+       mangle: false
      },
      dist: {
        files: {
          '<%= distDir %><%= jsDir %><%= pkg.name %>.min.js': ['<%= concat.js.dest %>'],
-         '<%= distDir %><%= jsDir %><%= bwr.name %>.min.js': ['<%= bower_concat.all.dest %>.js']
+         '<%= distDir %><%= jsDir %><%= bwr.name %>.min.js': ['<%= bower_concat.all.dest %>']
        }
      }
    },
@@ -50,7 +50,7 @@ module.exports = function (grunt) {
        },
        files: {
          '<%= distDir %><%= cssDir %><%= pkg.name %>.min.css': ['<%= concat.css.dest %>'],
-         '<%= distDir %><%= cssDir %><%= bwr.name %>.min.css': ['<%= bower_concat.all.cssDest %>.css']
+         '<%= distDir %><%= cssDir %><%= bwr.name %>.min.css': ['<%= bower_concat.all.cssDest %>']
        }
      }
    },
@@ -65,16 +65,22 @@ module.exports = function (grunt) {
      }
    },
    bower: {
-   install: {
-     options: {
-       targetDir: './web/lib/',
-       layout: function(type, component, source){
-         return type;
-       },
-       install: true,
+     dev: {
+       dest: '<%= tempDir %>',
+       fonts_dest: '<%= distDir %><%= fontsDir %>',
+       options: {
+         keepExpandedHierarchy: false
+       }
      }
    },
+   clean: {
+     build: ['<%= tempDir %>']
+   },
    watch: {
+      html: {
+        files: ['<%= devDir %><%= htmlDir %>**/*.html'],
+        tasks: ['copy']
+      },
       css: {
         files: ['<%= devDir %><%= cssDir %>**/*.css'],
         tasks: ['concat', 'cssmin']
@@ -84,7 +90,7 @@ module.exports = function (grunt) {
         tasks: ['concat', 'uglify']
       }
     },
-   nodemon: {
+    nodemon: {
       dev: {
         script: 'server.js'
       }
@@ -95,25 +101,27 @@ module.exports = function (grunt) {
       },
       tasks: ['nodemon', 'watch']
     }
-  }
-
   });
 
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-bower-task');
+  grunt.loadNpmTasks('grunt-bower');
   grunt.loadNpmTasks('grunt-bower-concat');
 
   grunt.registerTask('default', [
-    'concat', 'bower_concat',   // Concat JS and CSS in Web
-    'uglify', 'cssmin',         // Minify JS and CSS
-    'copy',                     // Copy HTML to Web
-    'bower'                      // Copy Bower Files
+    'concat', 'bower_concat',   // Concat JS and CSS and copy to Temp folder
+    'uglify', 'cssmin',         // Minify JS and CSS and copy to Web folder
+    'copy',                     // Copy HTML to Web folder
+    'bower',                    // Copy Bower components to Web folder
+    'clean',                    // Clean up Temp folder
+    'concurrent'                // Run Watch and Node concurrently
+
   ]);
 
 };
